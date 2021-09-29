2aba9bd8d727224ff6fd126376558e840788035e
YuriPiemontese <37075189+YuriPiemontese@users.noreply.github.com>
2021-09-29 07:47:34 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,13 +1,25 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  angleMode(DEGREES);
+  frameRate(8);
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  background("ffffff");
+
+  for (var i = 0; i < 1000; i++) {
+    fill("ffffff");
+    rect(random(-width / 2, width), random(0, height), width / 3, 5);
+    stroke("black");
+    strokeWeight(1.5);
+  }
+
+  colorMode(HSB);
+
+  for (var i = 0; i < 500; i++) {
+    rect(random(0, width), random(0, height), 6, 6);
+    fill(lerpColor(color(0, 100, 90), color(255, 100, 90), random(0, 1)));
+    stroke("black");
+    strokeWeight(1.5);
+  }
 }
